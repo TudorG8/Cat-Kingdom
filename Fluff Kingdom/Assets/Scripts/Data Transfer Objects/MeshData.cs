c1b7554f87119ae8521870745a5216ec071f9254
CAMPUS\tgheor <mr.tudor.gheorghe@gmail.com>
2018-12-04 11:07:15 +0000

    add fog of war script and mesh data

---
+++ Fluff Kingdom/Assets/Scripts/Data Transfer Objects/MeshData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData : MonoBehaviour {
+	[SerializeField] Vector3[] vertices;
+	[SerializeField] Color[] colours;
+	
+	public static MeshData FromMesh(Mesh filter) {
+		MeshData meshData = new MeshData();
+        meshData.vertices = filter.vertices;
+        meshData.colours = filter.colors;
+
+        return meshData;
+    }
+}
